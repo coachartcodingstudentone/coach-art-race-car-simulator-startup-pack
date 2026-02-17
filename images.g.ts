9871f367c8ea7ed9bfa815aa6c149c2650c2a439
coachartcodingstudentone <coachartcodingstudentone@gmail.com>
2026-02-16 19:05:01 -0500

    PIZZA

--- images.g.ts
+++ images.g.ts
@@ -485,5 +485,12 @@ cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
         return null;
     })
 
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
 }
 // Auto-generated code. Do not edit.
